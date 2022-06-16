ec1d3aa8bfadf24e298f2c64282505cf0ed4a867
jaylim92 <jaylim3272@gmail.com>
2022-06-16 22:36:15 +0900

    mixin

--- src/controllers/videoConroller.js
+++ src/controllers/videoConroller.js
@@ -4,7 +4,32 @@ const fakeUser = {
 };
 
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6];
+  const videos = [
+    {
+      title: "hello",
+      rating: 5,
+      comment: 2,
+      createAt: "1 hours ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "COOL",
+      rating: 5,
+      comment: 2,
+      createAt: "1 hours ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "I know what you've done before",
+      rating: 5,
+      comment: 5,
+      createAt: "1 hours ago",
+      views: 59,
+      id: 1,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
 
