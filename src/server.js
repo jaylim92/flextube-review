c770821d9e3b9d08b73fa67acb0f6199dd72d694
jaylim92 <jaylim3272@gmail.com>
2022-07-06 22:16:58 +0900

    .env 환경변수 설정

--- src/server.js
+++ src/server.js
@@ -18,11 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "good",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/flextube",
+      mongoUrl: process.env.DB_URL,
     }),
   })
 );
